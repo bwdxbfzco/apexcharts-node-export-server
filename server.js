e215c54954fa01eeb59331a612a80db611c9020e
bwdxbfzco <55143036+bwdxbfzco@users.noreply.github.com>
2025-02-02 06:04:05 +0400

    1st commit

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.post('/generate-chart', async (req, res) => {
         // Launch a Puppeteer browser instance
         const browser = await puppeteer.launch({
             executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
+            headless: 'new',
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
